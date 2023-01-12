5ebaeb0ea94d45dcec76fa49520fd97986a5bee3
Stephen <stephengift43@gmail.com>
2023-01-12 02:34:37 +0100

    Update index.tsx

--- containers/Homepage/index.tsx
+++ containers/Homepage/index.tsx
@@ -12,23 +12,23 @@ import SMDSummary from "./Summary";
 
 const SMDHomepageContainer = () => {
   return (
-    <Box pos="relative">
-      <Box
-        h="250px"
-        w="full"
-        bgColor={useColorModeValue("light.200", "dark.200")}
-        position="absolute"
-        top={0}
-        left={0}
-        zIndex="-1"
-      />
-      <Container maxW="container.xl">
-        <Flex flexDir="column" gap={9}>
-          <SMDHeader />
-          <SMDSummary />
-          <SMDOverview />
-        </Flex>
-      </Container>
+      <Box pos="relative">
+        <Box
+          h="250px"
+          w="full"
+          bgColor={useColorModeValue("light.200", "dark.200")}
+          position="absolute"
+          top={0}
+          left={0}
+          zIndex="-1"
+        />
+        <Container maxW="container.xl" pos="relative" pt={7}>
+          <Flex flexDir="column" gap={9}>
+            <SMDHeader />
+            <SMDSummary />
+            <SMDOverview />
+          </Flex>
+        </Container>
     </Box>
   );
 };
